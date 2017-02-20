42ddaa784c6a12f2ef0792ea699ca3e6a2b51717
Arun <aruntnj.av@gmail.com>
2017-02-20 14:20:05 +0530

    Third

--- src/com/example/sd/MainActivity.java
+++ src/com/example/sd/MainActivity.java
@@ -1,17 +1,38 @@
 package com.example.sd;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
 
+public class MainActivity extends Activity {
+	EditText etContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etContent = (EditText) findViewById(R.id.et_content);
+        OnClickListener saveClickListener = new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				File file = new File()
+			}
+        	
+        }
     }
 
     @Override
